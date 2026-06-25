9987466fc1bae95ca87f45f3e22a9b962a3566db
Not_Good <reetujoshi588@gmail.com>
2026-06-25 23:03:17 +0530

    Update avatar.js

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -4,12 +4,19 @@ const { createEmbed } = require('../../embedHelper');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('avatar')
-        .setDescription('Get a user\'s avatar.')
-        .addUserOption(option => option.setName('user').setDescription('The user to get the avatar from')),
+        .setDescription('Get a user\'s avatar')
+        .addUserOption(option => 
+            option.setName('target')
+            .setDescription('The user to get the avatar from')),
     async execute(interaction) {
-        // Fallback to the command user if no one is mentioned
-        const user = interaction.options.getUser('user') || interaction.user;
+        // Use 'target' as the option name, and fall back to interaction.user
+        const user = interaction.options.getUser('target') || interaction.user;
         
+        // Ensure we have a valid user object before calling displayAvatarURL
+        if (!user) {
+            return interaction.reply({ content: 'Could not find that user!', ephemeral: true });
+        }
+
         const embed = createEmbed(`${user.username}'s Avatar`, '')
             .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }));
             
