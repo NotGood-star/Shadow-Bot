79e7015c20b9c9cb234e3d1641daf52c9acb2607
Not_Good <reetujoshi588@gmail.com>
2026-06-25 22:59:22 +0530

    Update say.js

--- commands/utility/say.js
+++ commands/utility/say.js
@@ -1,19 +1,17 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { createEmbed } = require('../../embedHelper');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('say')
-        .setDescription('Make the bot say something.')
-        .addStringOption(option => 
-            option.setName('message')
-            .setDescription('The message to send')
-            .setRequired(true))
+        .setDescription('Make the bot say something in an embed.')
+        .addStringOption(option => option.setName('message').setDescription('The message').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     async execute(interaction) {
-        const message = interaction.options.getString('message');
+        const text = interaction.options.getString('message');
+        const embed = createEmbed('Announcement', text);
         
-        // Bot sends the message directly
-        await interaction.reply({ content: 'Message sent!', ephemeral: true });
-        await interaction.channel.send(message);
+        await interaction.reply({ content: 'Sent!', ephemeral: true });
+        await interaction.channel.send({ embeds: [embed] });
     },
 };
