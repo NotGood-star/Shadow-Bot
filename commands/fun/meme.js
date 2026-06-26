878fc83ddf08023b90b977eefd95ff07f734e5a5
Not_Good <reetujoshi588@gmail.com>
2026-06-26 09:56:54 +0530

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,21 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-// We use a free API to fetch random memes
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('meme')
         .setDescription('Get a random meme from Reddit'),
     async execute(interaction) {
-        const response = await fetch('https://meme-api.com/gimme');
-        const data = await response.json();
+        // 1. Tell Discord you are working on it so it doesn't time out
+        await interaction.deferReply();
 
-        const embed = new EmbedBuilder()
-            .setTitle(data.title)
-            .setURL(data.postLink)
-            .setImage(data.url)
-            .setFooter({ text: `Subreddit: ${data.subreddit}` });
+        try {
+            // 2. Fetch the meme
+            const response = await fetch('https://meme-api.com/gimme');
+            const data = await response.json();
 
-        await interaction.reply({ embeds: [embed] });
+            // 3. Create the embed
+            const embed = new EmbedBuilder()
+                .setTitle(data.title)
+                .setURL(data.postLink)
+                .setImage(data.url)
+                .setFooter({ text: `Subreddit: ${data.subreddit}` })
+                .setColor('#FF5733');
+
+            // 4. Edit the deferred reply with the actual result
+            await interaction.editReply({ embeds: [embed] });
+        } catch (error) {
+            console.error('MEME ERROR:', error);
+            await interaction.editReply({ content: 'I had trouble finding a meme!' });
+        }
     },
 };
